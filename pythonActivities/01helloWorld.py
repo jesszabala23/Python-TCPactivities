51841de2443da966f86332bc65731863db91d123
jesszabala23 <jzabala23@gmail.com>
2020-08-05 18:32:48 -0400

    first commit

---
+++ pythonActivities/01helloWorld.py
@@ -0,0 +1,7 @@
+#comments start with a hashtag in python files
+print("Hello, Friend!")
+print("Are you ready to learn python!")
+print("Even when the world gets bad I can turn to python!")
+print("We love Tech!")
+
+
---
+++ pythonActivities/05myIfElse.py
@@ -0,0 +1,18 @@
+is_foodie = False
+do_cook = False
+
+if is_foodie and do_cook:
+  print("Are you a foodie!")
+elif is_foodie and not(do_cook):
+  print("You are not a foodie.")
+else:
+  print("You are not a foodie and don't cook?!")
+
+
+# >, <, >=, <=, !=, ==
+if 1 > 3:
+  print("number omparison was true")
+
+
+if "dog" == "cat":
+  print("string omparison was true")
